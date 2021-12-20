0308470cd936d81bda5e4f5f5b566f6f568c6ce7
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2021-12-20 20:29:32 +0100

    du04: create initial functions for arrays

--- DomaciUloha4/main.c
+++ DomaciUloha4/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int array_new[1000000];
+int array_repeated[100000];
+int array_counter[100000];
+
+void array_in(){
+	for(int i = 0 ; i < 100000;i++){
+		array_counter[i] = 0;
+		array_repeated[i] = 0;
+	}
+	for(int i = 0 ; i < 1000000 ; i++){
+		array_new[i] = 0;
+	}
+}
+
+
 int main(void){
+
+	array_in();
+
 
 	return 0;
 }
