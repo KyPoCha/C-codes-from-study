c303ed25e890bc4b2214c882bf08a185fb030c89
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2021-12-20 21:08:25 +0100

    du07: created purge of entries and define MAX for arrays

--- DomaciUloha7/main.c
+++ DomaciUloha7/main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/* defining "MAX" for arrays of chars */
+#define MAX 10000
+/* global counter variable */
+long cnt = 0;
 int main(void){
+
+  char charArray[MAX];
+  long k;
+  char c;
+  printf("Pocet kostek:\n");
+  short input = scanf("%ld", &k);
+  /* processing of entries */
+  if(input != 1 || k <= 0){
+    printf("Nespravny vstup.\n");
+    return 1;
+  }
+  /* processing of numbers entries */
+  printf("Velikosti:\n");
+  long * array = (long*)calloc(k,sizeof(long));
+  for(long i = 0; i < k; i++){
+    if(scanf("%ld", &array[i]) != 1 || array[i] <= 0){
+      scanf("%c", &c);
+      if(c != ' ' || c != '\n'){
+        printf("Nespravny vstup.\n");
+        free(array);
+        return 1;
+      }
+      printf("Nespravny vstup.\n");
+      free(array);
+      return 1;
+    }
+  }
 
   return 0;
 }
