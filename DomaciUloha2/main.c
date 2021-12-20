ea42b587c581cdc759ccd95d4585ef55f79d8a1d
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2021-12-20 20:14:10 +0100

    du02: create main loops

--- DomaciUloha2/main.c
+++ DomaciUloha2/main.c
@@ -13,5 +13,32 @@ long long int nasobeni(long long a, long long b){
 }
 
 int main( void ){
+        long int a, b, res;
+        printf("Zadejte dve cisla:\n");
+        if(scanf("%ld %ld",&a,&b) != 2 || a<0 || b<=0){
+                printf("Nespravny vstup.\n");
+                return 1;
+        }
+        res = a / b;
+        printf(" %ld : %ld = %ld\n", a, b, res);
+
+	int i = 0;
+
+	for( i = 0 ; a/nasobeni(10,i) > 0; i ++){
+	}
+	i--;
+
+	long long int t = 0;
+	long long x;
+	int counter = 0;
+	int k = 2;
+	for(int g = i; g > -1; g --){
+		x = (a/nasobeni(10,g))%10;
+		if((t+x)/b == 0 && counter == 0){
+			t+=x;
+			t*=10;
+		}
+		k++;
+	}
         return 0;
 }
