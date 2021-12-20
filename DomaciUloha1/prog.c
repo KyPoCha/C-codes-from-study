8a7f38d06730ba25806e2320d9552604122b6f29
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2021-12-20 20:06:59 +0100

    du01: create check function for digits

--- DomaciUloha1/prog.c
+++ DomaciUloha1/prog.c
@@ -2,6 +2,12 @@
 #include <math.h>
 #include <float.h>
 typedef char* string;
+
+double Vodovodni(double la, double lb, double pa, double pb, double pre);
+double Kolmy(double la, double lb, double pa, double pb, double pre);
+int equal(double pa, double y);
+
 int main(void){
 return 0;
 }
+
