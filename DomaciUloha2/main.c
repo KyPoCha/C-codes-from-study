e5c6751307675aa7e7e82d95e4438a0757c3475a
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2021-12-20 20:13:14 +0100

    du02: create division function nasobeni()

--- DomaciUloha2/main.c
+++ DomaciUloha2/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 #include <math.h>
+
+long long int nasobeni(long long a, long long b){
+	long long t = a;
+	if(b == 0){
+		return 1;
+	}
+	for(int i = 1; i < b; i ++){
+		a*=t;
+	}
+	return a;
+}
+
 int main( void ){
         return 0;
 }
