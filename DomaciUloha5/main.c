2bae2af3d423e9867cebf6066e16537545da1c77
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2021-12-20 20:54:27 +0100

    du05: create purge of entries

--- DomaciUloha5/main.c
+++ DomaciUloha5/main.c
@@ -12,6 +12,31 @@ typedef struct itemList{
 	size_t size;
 } itemList_t;
 
+void add(itemList_t * itemsList, char *name){
 int main(void){
+	long tracked;
+	printf("Pocet sledovanych:\n");
+	if(scanf("%ld",&tracked) != 1 || tracked <= 0){
+		printf("Nespravny vstup.\n");
+		return 1;
+	}
+	printf("Pozadavky:\n");
+	short code;
+	char a[BUFFER_SIZE];
+	itemList_t items;
+	char funkce;
+	items.size = 0;
+	scanf("%c", &funkce);
+	while((code = scanf("%c", &funkce) != EOF)){
+		if(code != 1 || (funkce != '+' && funkce != '?' && funkce != '#')){
+			printf("Nespravny vstup.\n");
+			return 1;
+		}
+		if(funkce == '+'){
+			if(scanf("%99s", a) != 1){
+				printf("Nespravny vstup.\n");
+				return 1;
+			}
+			a[strlen(a)]='\0';
 	return 0;
 }
