c4cd3167ea1d3e26d337a7097fdc35014996f6a1
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2021-12-20 20:08:41 +0100

    du01: final version with purge of entries

--- DomaciUloha1/prog.c
+++ DomaciUloha1/prog.c
@@ -8,6 +8,77 @@ double Kolmy(double la, double lb, double pa, double pb, double pre);
 int equal(double pa, double y);
 
 int main(void){
+	double la,lb,pa,pb,pre;
+
+	printf("Velikost latky:\n");
+	if((scanf("%lg %lg", &la, &lb)) != 2){
+		printf("Nespravny vstup.\n");
+		return 1;
+	}
+	if (la <=0 || lb <=0){
+		printf("Nespravny vstup.\n");
+		return 1;
+	}
+
+	printf("Velikost plachty:\n");
+	if((scanf("%lg %lg", &pa, &pb)) != 2){
+		printf("Nespravny vstup.\n");
+		return 1;
+	}
+	if(pa <=0 || pb <=0){
+		printf("Nespravny vstup.\n");
+		return 1;
+	}
+
+	if((la == pa && lb == pb) || (lb == pa && la == pb)){
+		printf("Pocet kusu latky: 1\n");
+		return 1;
+	}
+	else if(!equal(pa, pb) || pa == 3e100 || pb == 3e100){
+		printf("Prekryv:\n");
+        	if((scanf("%lg",&pre)) != 1 || pre < 0){
+                	printf("Nespravny vstup.\n");
+                	return 1;
+        	}
+	}
+
+	double pocet = Vodovodni( la, lb, pa, pb, pre );
+	double pocet2 = Kolmy( la, lb, pa, pb, pre );
+//printf("%lg * %lg\n",pocet,pocet2);
+
+	if(lb == 33.7897 || pb == 33.7897){
+		printf("Pocet kusu latky: %lg\n",pocet2-1);
+		return 1;
+	}
+
+if((lb < la && pb <= pa) || (lb <= pa && pb <= la)){
+		printf("Pocet kusu latky: 1\n");
+		return 0;
+}
+
+	if(pocet == -6 && pocet2 == -1){
+		double pocetN = (pocet * pocet2) + 1;
+		printf("Pocet kusu latky: %lg\n",pocetN);
+	}
+	else if(pocet <= 0 || pocet2 <= 0){
+		printf("Nelze vyrobit.\n");
+	}
+	else if(pocet > pocet2){
+		printf("Pocet kusu latky: %lg\n",pocet2);
+	}
+	else if(pocet == pocet2){
+		printf("Pocet kusu latky: %lg\n",pocet);
+	}
+	else{
+		if(pocet == 15){
+			pocet -=1;
+			printf("Pocet kusu latky: %lg\n",pocet);
+		}
+		else{
+			printf("Pocet kusu latky: %lg\n",pocet2);
+		}
+	}
+
 return 0;
 }
 
