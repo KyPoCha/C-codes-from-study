e2d7203f9a1f6c10884f45bbeae9fc3d20dad395
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2021-12-20 20:30:21 +0100

    du04: resolved task of "+" function and purge of entries

--- DomaciUloha4/main.c
+++ DomaciUloha4/main.c
@@ -20,6 +20,36 @@ int main(void){
 
 	array_in();
 
+	int counter = 0, id;
+	char funkce;
+	int to;
+
+
+	short code;
+	printf("Pozadavky:\n");
+	while((code = scanf("%c %d", &funkce, &id)) != EOF){
+		if(code != 2 || (funkce != '+' && funkce != '?') || id < 0){
+			printf("Nespravny vstup.\n");
+			return 1;
+		}
+		if(funkce == '+'){
+			if(id > 99999){
+				printf("Nespravny vstup.\n");
+				return 1;
+			}
+			array_new[counter] = id;
+			array_repeated[id] +=1;
+
+			if(array_repeated[id] == 1){
+				printf("> prvni navsteva\n");
+			}
+			else{
+				printf("> navsteva #%d\n",array_repeated[id]);
+			}
+
+			counter++;
+		}
+	}
 
 	return 0;
 }
