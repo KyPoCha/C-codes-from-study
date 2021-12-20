8818237a05d3f948de43db5b68ee45877c963ab1
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2021-12-20 20:53:21 +0100

    du05: create structurs for products lists

--- DomaciUloha5/main.c
+++ DomaciUloha5/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+typedef struct item{
+	char name[BUFFER_SIZE];
+	long count;
+} item_t;
+
+typedef struct itemList{
+	item_t **items;
+	size_t size;
+} itemList_t;
+
 int main(void){
 	return 0;
 }
