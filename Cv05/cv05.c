f35d899d49706876e96b82a428150b0663ebda5c
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2021-12-21 11:58:42 +0100

    cv05: purge of entries

--- Cv05/cv05.c
+++ Cv05/cv05.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 int main()
 {
+    printf("Zadejte pocet poli:\n");
+    if(scanf("%d",&len) != 1 || len <= 0){
+        printf("Nespravny vstup.\n");
+        return 1;
+    }
+    printf("Zadejte velikost pole:\n");
+    if(scanf("%d",&pocet) != 1 || pocet <=0){
+        printf("Nespravny vstup.\n");
+        return 1;
+    }
+
+
+
+
 return 0;
 
 }
