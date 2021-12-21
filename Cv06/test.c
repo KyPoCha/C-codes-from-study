7fbf0e12b66d1db634abb71606bcbc57fefe4ef0
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2021-12-21 12:04:52 +0100

    cv06: initial commit

---
+++ Cv06/test.c
@@ -0,0 +1,17 @@
+#ifndef __PROGTEST__
+#include <stdio.h>
+#include <limits.h>
+#include <assert.h>
+#include <stdint.h>
+#endif /* __PROGTEST__ */
+
+long long avg3 ( long long a, long long b, long long c )
+{
+}
+
+#ifndef __PROGTEST__
+int main (int argc, char * argv [])
+{
+  return 0;
+}
+#endif /* __PROGTEST__ */
