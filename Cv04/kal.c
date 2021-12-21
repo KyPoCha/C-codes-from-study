103a0ea61c1573067039a30765120ac60a2045f1
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2021-12-21 11:54:39 +0100

    cv04: create main function for counting

--- Cv04/kal.c
+++ Cv04/kal.c
@@ -1,6 +1,31 @@
 #include<stdio.h>
 #include<math.h>
 int main(void){
+        double x,y;
+        char f,r;
+        printf("Zadejte vzorec:\n");
+                switch(f){
+                        case '+':
+                                printf("%d\n",(int)(rint(x+y)));
+                                break;
+                        case '-':
+                                printf("%d\n",(int)(rint(x-y)));
+                                break;
+                        case '*':
+                                printf("%g\n",x*y);
+                                break;
+                        case '/':
+                                if(y==0){
+                                printf("Nespravny vstup.\n");
+                                }
+                                else
+                                {
+                                printf("%.f\n",(double)(x)/y);
+                                }
+                                break;
+                        default : printf("Nespravny vstup.\n");
+                }
+        }
         return 0;
 
 }
