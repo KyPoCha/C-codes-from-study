1fa81cea1121e62d8025301e28a3b9801b7acfd5
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2021-12-21 12:07:26 +0100

    cv06: create min() function

--- Cv06/test.c
+++ Cv06/test.c
@@ -20,8 +20,24 @@ long long max( long long a, long long b, long long c)
 }
 
 
+long long min( long long a, long long b, long long c)
+{
+  if (a <= b && a <= c){
+	return a;
+  }
+  if (b <= a && b <= c){
+	return b;
+  }
+  if (c <= a && c <=b){
+	return c;
+  }
+  return 1;
+}
+
+
 long long avg3 ( long long a, long long b, long long c )
 {
+  return LLONG_MAX;
   }
 }
 
