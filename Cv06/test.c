c8abd388c54b837484ca71e1d4b841e6d0b9ee5d
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2021-12-21 12:05:24 +0100

    cv06: added asserts

--- Cv06/test.c
+++ Cv06/test.c
@@ -12,6 +12,14 @@ long long avg3 ( long long a, long long b, long long c )
 #ifndef __PROGTEST__
 int main (int argc, char * argv [])
 {
+  assert ( avg3 ( 9223372036854775800, 9223372036854775800, 9223372036854775800 ) == 9223372036854775800);
+  assert( avg3 ( 9223372036854775800, 4611686018427387897, 3 ) == 4611686018427387900);
+
+  assert ( avg3 ( 1, 2, 3 ) == 2 );
+  assert ( avg3 ( -100, 100, 30 ) == 10 );
+  assert ( avg3 ( 1, 2, 2 ) == 1 );
+  assert ( avg3 ( -1, -2, -2 ) == -1 );
+  assert ( avg3 ( LLONG_MAX, LLONG_MAX, LLONG_MAX ) == LLONG_MAX );
   return 0;
 }
 #endif /* __PROGTEST__ */
